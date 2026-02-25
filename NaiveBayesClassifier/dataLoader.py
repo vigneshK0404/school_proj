6b1407ad26a1cca18670cb4c5ac7210230f019ae
Vignesh <vigneshwar.karthik04@gmail.com>
2026-02-25 17:53:35 -0500

    Finished Language processing

--- NaiveBayesClassifier/dataLoader.py
+++ NaiveBayesClassifier/dataLoader.py
@@ -4,32 +4,43 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 
+
+#RUN ONCE
+#import nltk
+#nltk.download("punkt")
+#nltk.download('stopwords')
+
 class DataLoader():
     def __init__(self, file_path : str):
         with open(file_path,"rt") as f:
             raw_input = f.read().lower()
-            lines = re.sub(pattern = r'[^\w\s]', repl = "", string = raw_input)
-            lines = raw_input.splitlines()
+
             
         self.labels = []
         self.data = []
+        pS = PorterStemmer()
 
+        stopWords = set(stopwords.words('english'))
+
+        lines = re.sub(r'[^\w\s]','',raw_input)
+        lineList = lines.splitlines()
         
-        
-        for entry in lines:
+        for entry in lineList:
             #print(entry)
             if (not entry.startswith("ham")) and (not entry.startswith("spam")):
                 continue
             
             entrySplit = entry.split("\t")
             self.labels.append(entrySplit[0])
-            self.data.append(entrySplit[-1])
+            tokenList = word_tokenize(entrySplit[-1], language = "english", preserve_line = False)
+            self.data.append([pS.stem(word) for word in tokenList if word not in stopWords])
+
 
 
 
 
 if __name__  == "__main__":
     dL = DataLoader("SMSSpamCollection.txt")
-    print(dL.data[-2])
     print(dL.labels[-2])
+    print(dL.data[-2])
 
