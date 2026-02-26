ba229ed43d8a9d4b91598fcf0fe931053fb234d3
Vignesh <vigneshwar.karthik04@gmail.com>
2026-02-26 11:34:02 -0500

    Finalized dataLoader minus perchance numpy

--- NaiveBayesClassifier/dataLoader.py
+++ NaiveBayesClassifier/dataLoader.py
@@ -3,6 +3,7 @@ from collections import defaultdict, Counter
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
+import random
 
 
 #RUN ONCE
@@ -35,12 +36,28 @@ class DataLoader():
             tokenList = word_tokenize(entrySplit[-1], language = "english", preserve_line = False)
             self.data.append([pS.stem(word) for word in tokenList if word not in stopWords])
 
+    def load_data(self):
+        self.labels = [0 if label == "ham" else 1 for label in self.labels]
+        
+    def split_data(self):
+        tempidx = list(range(len(self.labels)))
+        random.shuffle(tempidx)
+        tempLabels = []
+        tempData = []
+        for idx in tempidx:
+            tempLabels.append(self.labels[idx])
+            tempData.append(self.data[idx])
 
-
+        self.labels = tempLabels
+        self.data = tempData
 
 
 if __name__  == "__main__":
     dL = DataLoader("SMSSpamCollection.txt")
-    print(dL.labels[-2])
-    print(dL.data[-2])
+    print(f"{dL.labels[-1]} : {dL.data[-1]}")
+    dL.load_data()
+    print(f"{dL.labels[-1]} : {dL.data[-1]}")
+    dL.split_data()
+    print(f"{dL.labels[-1]} : {dL.data[-1]}")
+
 
