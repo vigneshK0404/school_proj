d9b138aaa64b1c03d17b49fae1be1d19af4f056b
Vignesh <vigneshwar.karthik04@gmail.com>
2026-05-08 11:36:23 -0400

    figured out pdfreader a little

--- Fintracker/fintracker.py
+++ Fintracker/fintracker.py
@@ -1,5 +1,20 @@
 from PyPDF2 import PdfReader
 
+
+
+def visitor_body(text, cm, tm, font_dict, font_size):
+    print(f"text : {text}")
+    print(f"cm : {cm}")
+    print(f"tm : {tm}")
+    y = tm[5]
+    parts = []
+    #if 50 < y < 720 or y == 0:
+    parts.append(text)
+
+    return parts
+
+
 reader = PdfReader("statement.pdf")
 page = reader.pages[2]
-print(page.extract_text())
+parts = page.extract_text(visitor_text=visitor_body)
+#print(parts)
