f862846e73fe29dca08e2b6428856f06f2e4d7c9
Vignesh <vigneshwar.karthik04@gmail.com>
2026-05-16 12:15:47 -0400

    Added YAML check and new class for bookkeeping, have not run yet neet ot check correctness

--- Fintracker/fintracker.py
+++ Fintracker/fintracker.py
@@ -10,13 +10,8 @@ class BluePrint(ABC):
         pass
 
     @abstractmethod
-    def addYAML(self,tmp):
-        pass
-
-    @abstractmethod
     def categorize(self):
         pass
-
 
 
 class Financial_Tracker(BluePrint):
@@ -40,9 +35,6 @@ class Financial_Tracker(BluePrint):
 
         self.df["Description"] = self.df["Description"].str.replace(r'[^A-Za-z\s]','',regex=True).str.strip()
         return
-        
-    def addYAML(self,tmp):
-        pass
     
     def categorize(self):
         idxs = []
@@ -61,15 +53,15 @@ class Financial_Tracker(BluePrint):
             self.transactions[key] = total
 
         misc = self.df["Amount"].sum()
-        tmp= self.df.drop(idxs)   
+        leftover = self.df.drop(idxs)   
 
         #TODO: pass tmp into add YAML and give user the choice to choose what category, add new category or add to misc if error
         
         #print(tmp)
         #print(self.df)
 
-        return self.transactions
-
+        return self.transactions, leftover        
+        
 
 
 
@@ -93,7 +85,63 @@ class Discover(Financial_Tracker):
     def clean_csv(self):
         self.df = self.df[["Post Date","Description","Amount"]].rename(columns={"Post Date": "Date"})
         super().clean_csv()
-        return       
+        return    
+
+
+class manageTransactions():
+    def __init__(self,transactions : dict, leftover : pd.DataFrame, categoryPath : str):
+        self.transactions = transactions
+        self.leftover = leftover
+        self.categoryPath = categoryPath
+
+        with open(categoryPath) as stream:
+            try:
+                self.categories = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+
+    def __add__(self,other):
+        retT = {}
+        for key in self.transactions.keys():
+            retT[key] = self.transactions[key] + other.transactions[key]
+
+        retL = pd.concat([self.leftover,other.leftover])
+
+        return retT, retL
+
+   
+    def tally(self):
+        dfLen = len(self.leftover)
+        if dfLen == 0:
+            print("Nothing to Tally")
+            return
+
+        print("Existing Categories")
+        print(self.transactions.keys())
+
+        
+        for i in range(dfLen):
+            desc = self.leftover["Description"].iloc[i]
+            category = input(f"Category for {desc} : ")
+
+            if category in self.categories.keys():
+                self.categories[category].append(desc)
+            else:
+                self.categories[category] = [desc]
+
+            if category in self.transacrions.keys():
+                self.transactions[category] += self.leftover["Amount"].iloc[i]
+            else:
+                self.transactions[category] = self.leftover["Amount"].iloc[i]
+
+
+        with open(categoryPath, 'w') as file:
+            yaml.dump(self.categories, file, default_flow_style=False)
+
+
+    def displayTrans(self):
+        pass
 
 
 
